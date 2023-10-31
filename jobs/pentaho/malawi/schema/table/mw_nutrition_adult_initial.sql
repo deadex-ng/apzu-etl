4abcc7a953dc514699b5901d82c9035afe5c106c
deadex-ng <thisisfumba@gmail.com>
2023-10-31 15:29:56 +0200

    MLW-1554: Update nutrition tables to have separate columns for enrollment reasons

--- jobs/pentaho/malawi/schema/table/mw_nutrition_adult_initial.sql
+++ jobs/pentaho/malawi/schema/table/mw_nutrition_adult_initial.sql
@@ -1,10 +1,12 @@
-DROP TABLE IF EXISTS `mw_nutrition_adults_initial`;
+DROP TABLE IF EXISTS mw_nutrition_adults_initial;
 CREATE TABLE mw_nutrition_adults_initial (
     nutrition_initial_visit_id INT NOT NULL AUTO_INCREMENT,
     patient_id INT NOT NULL,
     visit_date DATE,
     location VARCHAR(255),
-    enrollment_reason VARCHAR(255),
+    enrollment_reason_tb VARCHAR(255),
+    enrollment_reason_hiv VARCHAR(255),
+    enrollment_reason_ncd VARCHAR(255),
     PRIMARY KEY (nutrition_initial_visit_id)
 );
 
